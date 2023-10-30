5f0a8642d473dad28d1060de0a27cf3f53b16f53
harsha4411 <harsha4411>
2023-10-30 11:57:29 +0530

    Second Commit

--- Demo11/src/pack1/Welcome.java
+++ Demo11/src/pack1/Welcome.java
@@ -1,8 +1,10 @@
 package pack1;
 
-
-public class Welcome {
-	public static void main(String args[]){
-	System.out.println("Hello");
+public class Welcome 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello");
+		System.out.println("Welcome to CSE");
 	}
 }
